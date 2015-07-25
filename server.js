201c8d4d2bf1eeffd203ad012d6355d975c81f64
Yuri Drabik <yurist38@mail.ru>
2015-07-25 14:30:29 +0700

    Server settings

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express'),
 app = express();
-app.use(express.static('www'));
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return next();
+  });
+app.use(express.static(path.join(application_root, "StaticPages")));
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));

adea93cbdeb586db6c7329695ddee0543669250c
Yuri Drabik <yurist38@mail.ru>
2015-07-26 22:29:06 +0700

    Infinite feed

--- www/js/app.js
+++ www/js/app.js
@@ -18,25 +18,43 @@ angular.module('InstaWall', ['ionic'])
 .controller("MainCtrl", function($scope, $http, $log){
 
   $scope.$log = $log;
-  var accessToken = "180849786.02eb1da.3d7bb82737ce4604ae16a84aa83142c5";
+  var accessToken = "180849786.02eb1da.3d7bb82737ce4604ae16a84aa83142c5"
   // FULL URI - https://api.instagram.com/v1/tags/{tag-name}?access_token=ACCESS-TOKEN
-  var mainUrl = "https://api.instagram.com/v1/tags/"
-  var fullUrl = mainUrl;
+    , MAIN_URL = "https://api.instagram.com/v1/tags/"
+    , maxTagId = '';
+
+  $scope.feedActive = false;
+
 
   $scope.findPics = function(){
-    mainUrl += $scope.tagname+"/media/recent?access_token="+accessToken+'&callback=JSON_CALLBACK&count=10';
-    $log.log(mainUrl);
+    firstUrl = MAIN_URL+$scope.tagname+"/media/recent?access_token="+accessToken+'&callback=JSON_CALLBACK&count=5';
 
-    var instaResponse = $http.jsonp(mainUrl);
+    var instaResponse = $http.jsonp(firstUrl);
 
     instaResponse.then(function(response) {
         $scope.instaData = response.data.data;
-        //$log.log($scope.instaData);
+        maxTagId = response.data.pagination.next_max_tag_id;
+        $scope.feedActive = true;
     });
     instaResponse.error(function(data, status, headers, config) {
         alert("Sorry! Can not connect with server... Try later.");
     });
 
+  }
+
+  $scope.loadMore = function() {
+    if(maxTagId){
+      var instaResponse = $http.jsonp(firstUrl+'&max_tag_id='+maxTagId)  ;
+      instaResponse.then(function(responseMore) {
+          var oldData = responseMore.data.data;
+          $scope.instaData = $scope.instaData.concat(oldData);
+          maxTagId = responseMore.data.pagination.next_max_tag_id;
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+      instaResponse.error(function(data, status, headers, config) {
+          alert("Sorry! Can not connect with server... Try later.");
+      });
+    }
   }
 
 });
